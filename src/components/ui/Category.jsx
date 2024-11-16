18efe12743af9708d0e1861ec2a866cbedc2206e
fahad-k-c <connect.fahadkc@gmail.com>
2024-11-16 13:52:07 +0530

    small change

--- src/components/ui/Category.jsx
+++ src/components/ui/Category.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Categ } from "../../shared/data";
+import { Categ } from "../../shared/Data";
 import { Link } from "react-router-dom";
 
 const Category = () => {
